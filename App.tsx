e0c2c2616d05ca41fecd9a1a55866a5f4b342b39
viniciusguido <vinivaba@gmail.com>
2023-11-13 23:11:37 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -14,9 +15,11 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  Button,
 } from 'react-native';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,6 +27,8 @@ const App = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const buttons = ['AC', 'DEL', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '3', '2', '1', '+', '0', '.', '+/-', '='
+  ];
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -36,19 +41,52 @@ const App = () => {
         style={backgroundStyle}>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Text style={styles.title}>Calculadora</Text>
+        </View>
+        <View style={styles.results}>
+          <Text style={{fontSize: 20, margin: 10}}>2+2=5</Text>
+        </View>
+        <View style={styles.buttons}>
+          {buttons.map(button =>(
+            <TouchableOpacity key={button} style={styles.button}>
+              <Text style={styles.textButton}>{button}</Text>
+            </TouchableOpacity>
+          ))};
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   title: {
     fontSize: 25,
     textAlign: 'center',
+  },
+  results: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    width: '100%',
+    height: '300',
+    backgroundColor: 'cyan',
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    flex: 2,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: 'white',
+    minHeight: '80',
+    minWidth: '80',
+  },
+  textButton: {
+    color: '#5b5b5b',
+    fontSize: 25,
   },
 });
 
