d963268f3eff8ea1204f7ac90b494338a9a41ebe
viniciusguido <vinivaba@gmail.com>
2023-11-19 15:08:25 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
+  Dimensions,
   StatusBar,
   StyleSheet,
   Text,
@@ -19,6 +19,9 @@ import {
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -45,8 +48,16 @@ const App = () => {
     '+',
     '0',
     '.',
-    '+/-',
+    '',
     '=',
+  ];
+
+  const calc_buttons = [
+    ['AC', 'DEL', '%', '/'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['3', '2', '1', '+'],
+    ['0', '.', '', '='],
   ];
 
   const [currentNumber, setCurrentNumber] = useState('');
@@ -93,9 +104,6 @@ const App = () => {
       setCurrentNumber(currentNumber + buttonPressed);
       return;
     }
-    if (buttonPressed === '+/-') {
-      return;
-    }
     if (buttonPressed === 'AC') {
       setLastNumber('');
       setCurrentNumber('');
@@ -111,13 +119,6 @@ const App = () => {
   };
 
   useEffect(() => {
-    // isso aqui limita o quanto vai atualizar o current number, para no máximo 20 caracteres
-    // length representa o tamanho do texto presente dentro do currentNumber
-    if (currentNumber.length > 0 && currentNumber.length < 20) {
-      // Esse trecho cuida da atualização dos números em tempo real, evitando aquele erro de loop infinito, porque se der algum erro, então esse trecho trata o erro (na maioria dos casos)
-      setCurrentNumber(currentNumber.substring(0, currentNumber.length - 1));
-    }
-
     // a linha de baixo, atualiza o currentNumber sempre que ele sofrer alguma alteração
   }, [currentNumber]);
 
@@ -127,37 +128,59 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+      <View style={styles.container}>
+        <View>
           <Text style={styles.title}>Calculadora</Text>
         </View>
+
         <View style={styles.results}>
           <Text style={styles.historyText}>{lastNumber}</Text>
           <Text style={styles.resultText}>{currentNumber}</Text>
         </View>
-        <View style={styles.buttons}>
-          {buttons.map(button => {
-            return (
-              <TouchableOpacity
-                style={styles.button}
-                key={button}
-                onPress={() => handleInput(button)}>
-                <Text style={styles.textButton}> {button}</Text>
-              </TouchableOpacity>
-            );
-          })}
+        <View style={styles.button_container}>
+          <View style={styles.buttons}>
+            {buttons.map((button: string) => {
+              // Esse trecho não vai rodar
+              if (button === '=') {
+                // É preciso tratar o button = de uma forma um pouco diferente
+                return (
+                  <TouchableOpacity
+                    style={[
+                      styles.button,
+                      {
+                        backgroundColor: '#ffbf60',
+                        borderRadius: 50,
+                      },
+                    ]}
+                    key={button}
+                    onPress={() => handleInput(button)}>
+                    <Text style={styles.textButton}> {button}</Text>
+                  </TouchableOpacity>
+                );
+              }
+              // o handle input é chamado sempre que um botão é clicado com o onPress
+              // para deixar os botões dinâmicos, é preciso passar a variável button no input
+              // Só esse trecho vai rodarr
+              return (
+                <TouchableOpacity
+                  style={styles.button}
+                  key={button}
+                  onPress={() => handleInput(button)}>
+                  <Text style={styles.textButton}> {button}</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   title: {
     fontSize: 25,
     textAlign: 'center',
@@ -166,8 +189,12 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
     width: '100%',
-    height: 300,
+    height: windowHeight / 2.5,
     backgroundColor: 'cyan',
+  },
+  button_container: {
+    width: windowWidth,
+    height: windowHeight,
   },
   buttons: {
     flexDirection: 'row',
@@ -178,8 +205,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignContent: 'center',
     backgroundColor: 'white',
-    minHeight: 80,
-    minWidth: 80,
+    minWidth: windowWidth / 4,
+    minHeight: windowHeight / 9,
   },
   textButton: {
     color: '#5b5b5b',
